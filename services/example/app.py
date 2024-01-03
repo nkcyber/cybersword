c50785354d5b2f6c9a13e975cf01f024579fdde6
Zack Sargent <zack@zack.fyi>
2024-01-03 15:47:27 -0500

    [WIP] add example service routing from nginx

--- services/example/app.py
+++ services/example/app.py
@@ -1,23 +1,10 @@
 import time
+from datetime import datetime
 
-import redis
 from flask import Flask
 
 app = Flask(__name__)
-cache = redis.Redis(host='redis', port=6379)
 
-def get_hit_count():
-    retries = 5
-    while True:
-        try:
-            return cache.incr('hits')
-        except redis.exceptions.ConnectionError as exc:
-            if retries == 0:
-                raise exc
-            retries -= 1
-            time.sleep(0.5)
-
-@app.route('/')
+@app.route('/hello')
 def hello():
-    count = get_hit_count()
-    return 'Hello World! I have been seen {} times.\n'.format(count)
+    return f'Hello World! {datetime.now().isoformat()}\n'
